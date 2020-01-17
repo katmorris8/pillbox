a9cd9244ca4b2518079fd1646cc03d91b5ea32a7
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-01-17 17:41:38 -0500

    added constructor and state.

--- src/client/App.js
+++ src/client/App.js
@@ -3,9 +3,8 @@ import './app.css';
 
 export default class App extends Component {
   constructor(props) {
-    super(props) {
-      this.state = null;
-    }
+    super(props);
+    this.state = null;
   }
   // state = { username: null };
 
