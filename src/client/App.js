45d24ffdf4c84982b1f56db94caeebe3ddd5d82b
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-01-17 17:39:02 -0500

    replaced boilerplate headline with app welcome message.

--- src/client/App.js
+++ src/client/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './app.css';
 
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  constructor(props) {
+    super(props) {
+      this.state = null;
+    }
   }
+  // state = { username: null };
+
+  // componentDidMount() {
+  //   fetch('/api/getUsername')
+  //     .then(res => res.json())
+  //     .then(user => this.setState({ username: user.username }));
+  // }
   // remove username
 
   render() {
