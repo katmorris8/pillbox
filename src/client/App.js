487cb3e4a6ffcc7e1d43e81652b751ff5f45021c
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-01-20 12:10:45 -0500

    Added a router link to the homepage when the view pillbox button is clicked.

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './app.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AddPillButton from './components/AddPillButton';
 import PillboxButton from './components/PillboxButton/PillboxButton';
+import MyPillbox from './components/MyPillbox/MyPillbox';
 
 export default class App extends Component {
   constructor(props) {
@@ -21,8 +23,13 @@ export default class App extends Component {
     return (
       <div>
         <h1>welcome to pillbox</h1>
-        <AddPillButton />
-        <PillboxButton />
+        <Router>
+          <AddPillButton />
+          <Link to="/mypillbox">
+            <PillboxButton />
+          </Link>
+          <Route path="/mypillbox" exact render={props => <MyPillbox {...props} />} />
+        </Router>
       </div>
     );
   }
