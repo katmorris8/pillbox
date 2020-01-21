ccaa9ab22e6e6fafc4a95e25308663014cacc4dc
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-01-20 19:54:36 -0500

    Moved welcome code from app to the welcome page.

--- src/client/components/WelcomePage/WelcomePage.js
+++ src/client/components/WelcomePage/WelcomePage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import './style.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AddPillButton from '../AddPillButton';
+import PillboxButton from '../PillboxButton/PillboxButton';
+import MyPillbox from '../MyPillbox/MyPillbox';
 
 export default class WelcomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+  
   render() {
     return (
-      <div>
-        
+        <div>
+        <h1>welcome to pillbox</h1>
+        <Router>
+          <AddPillButton />
+          <Link to="/mypillbox">
+            <PillboxButton />
+          </Link>
+          <Switch>
+            <Route path="/mypillbox" exact render={props => <MyPillbox {...props} />} />
+          </Switch>
+        </Router>
       </div>
     )
   }
