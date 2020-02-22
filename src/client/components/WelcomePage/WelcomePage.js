69fd46bd61393ce3d84d344ddf1dc58ad441231a
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-22 15:56:45 -0500

    Added click methods for the add pill button and view pillbox button to route the the respective pages.

--- src/client/components/WelcomePage/WelcomePage.js
+++ src/client/components/WelcomePage/WelcomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
-import AddPillButton from '../AddPillButton';
+import AddPillButton from '../AddPillButton/AddPillButton';
 import PillboxButton from '../PillboxButton/PillboxButton';
 import { Logo } from '../Logo/Logo';
 
@@ -10,13 +10,26 @@ export default class WelcomePage extends Component {
     this.state = null;
   }
 
+  handleAddPillClick = (e) => {
+    e.preventDefault();
+    console.log('hiii');
+    this.props.history.push('/newpill');
+  }
+
+  // move to app
+  handlePillboxClick = (e) => {
+    e.preventDefault();
+    console.log('hello!');
+    this.props.history.push('/mypillbox');
+  }
+
   render() {
     return (
       <div>
         <Logo />
         <h1>welcome to pillbox</h1>
-        <AddPillButton />
-        <PillboxButton />
+        <AddPillButton onClick={this.handleAddPillClick} />
+        <PillboxButton onClick={this.handlePillboxClick} />
       </div>
     );
   }
