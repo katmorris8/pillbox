3d67a4700eb05d9414921282cae15222dae4f654
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-23 15:14:24 -0500

    Changed the render back to app instead of router.

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './components/Router/Router';
+import App from './components/App/App';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
