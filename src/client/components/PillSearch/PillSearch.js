9192ee5e812f6afdc1537d9dc3dbda97363c5bc2
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-23 15:17:17 -0500

    Started the form functionality.

--- src/client/components/PillSearch/PillSearch.js
+++ src/client/components/PillSearch/PillSearch.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react';
 export default class PillSearch extends Component {
   constructor(props) {
     super(props);
+    this.state = null
+  }
 
-    this.state = null;
+  handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // const name = 
+    // this.setState({
+    //   name:
+    // });
   }
 
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="Pill name" />
+          <input type="text" placeholder="Pill name..." onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>></button>
         </form>
       </div>
     );
