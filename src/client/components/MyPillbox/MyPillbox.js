1427e65e34b3b973ace910f8fd0abcdf00c8f4e4
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-23 15:06:49 -0500

    Added the click function to the add pill button instance.

--- src/client/components/MyPillbox/MyPillbox.js
+++ src/client/components/MyPillbox/MyPillbox.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import AddPillButton from '../AddPillButton';
+import AddPillButton from '../AddPillButton/AddPillButton';
 
 export default class MyPillbox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isEmpty: true
-    };
+    this.state = null;
   }
 
   render() {
     return (
       <div>
-        {
-          this.state.isEmpty &&
-          <div className="empty-pillbox-message">
-            <h2>your pillbox is currently empty!</h2>
-            <AddPillButton />
-          </div>
-        }
+
+        <div className="empty-pillbox-message">
+          <h2>your pillbox is currently empty!</h2>
+          <AddPillButton addPillClick={this.props.addPillClick} />
+        </div>
+
       </div>
     );
   }
