bd5d95484a4543a35d46bf0d322edbb5c3076316
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-23 14:55:18 -0500

    Removed the button methods so the methods could live in the app and passed the methods as props to the add pill and view pillbox buttons.

--- src/client/components/WelcomePage/WelcomePage.js
+++ src/client/components/WelcomePage/WelcomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './style.css';
 import AddPillButton from '../AddPillButton/AddPillButton';
 import PillboxButton from '../PillboxButton/PillboxButton';
-import { Logo } from '../Logo/Logo';
 
 export default class WelcomePage extends Component {
   constructor(props) {
@@ -10,26 +9,12 @@ export default class WelcomePage extends Component {
     this.state = null;
   }
 
-  handleAddPillClick = (e) => {
-    e.preventDefault();
-    console.log('hiii');
-    this.props.history.push('/newpill');
-  }
-
-  // move to app
-  handlePillboxClick = (e) => {
-    e.preventDefault();
-    console.log('hello!');
-    this.props.history.push('/mypillbox');
-  }
-
   render() {
     return (
       <div>
-        <Logo />
         <h1>welcome to pillbox</h1>
-        <AddPillButton onClick={this.handleAddPillClick} />
-        <PillboxButton onClick={this.handlePillboxClick} />
+        <AddPillButton addPillClick={this.props.addPillClick} />
+        <PillboxButton viewPillboxClick={this.props.viewPillboxClick} />
       </div>
     );
   }
