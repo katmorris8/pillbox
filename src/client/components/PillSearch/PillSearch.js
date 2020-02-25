a211d15a946ad9f0cc7fdc8a0e74bf902d24ba27
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-02-24 21:24:19 -0500

    Handled the form input to save the pill name to state.

--- src/client/components/PillSearch/PillSearch.js
+++ src/client/components/PillSearch/PillSearch.js
@@ -3,17 +3,34 @@ import React, { Component } from 'react';
 export default class PillSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = null;
+    this.state = {
+      name: ''
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addPill(this.state);
+    this.setState({
+      name: ''
+    });
+  }
+
+  pillName = (e) => {
+    this.setState({
+      name: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.props.getPillName}>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Pill name..."
-            onChange={this.props.pillName}
+            onChange={this.pillName}
+            value={this.state.name}
           />
           <button type="submit" onClick={this.handleSubmit}>></button>
         </form>
