81de19ff5a152d612fbcc092bba39b01a763228c
Kat Morris <kathryn.leigh.morris@gmail.com>
2020-03-01 21:21:43 -0500

    Used sequelize for the prescriptions model.

--- src/server/models/prescription.js
+++ src/server/models/prescription.js
@@ -1,10 +1,23 @@
-const prescriptionModel = {
-  all: () => {
-    return { prescriptions: [{name: 'advil'}, {name: 'preparation h'}] }
-  },
-  create: (params) => {
-    return {status: 'okay', prescription: {id: 1, name: params.name}}
-  }
-};
+const Sequelize = require('sequelize');
 
-module.exports = prescriptionModel;
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:8080/pillbox_db', {
+  dialect: 'progres'
+});
+
+const Prescription = sequelize.define('prescription', {
+  name: Sequelize.TEXT
+});
+
+// const prescriptionModel = {
+// all: () => {
+//   return { prescriptions: [{name: 'advil'}, {name: 'preparation h'}] }
+// },
+// create: (params) => {
+//   return {status: 'okay', prescription: {id: 1, name: params.name}}
+// }
+// };
+
+module.exports = {
+  Prescription,
+  sequelize
+};
