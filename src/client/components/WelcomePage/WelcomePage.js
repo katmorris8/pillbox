7dad0242d0cfd7e00ad7f5c21d652b856165230d
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-07 20:42:58 -0500

    refactor component to functional component and add prop types

--- src/client/components/WelcomePage/WelcomePage.js
+++ src/client/components/WelcomePage/WelcomePage.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 import AddPillButton from '../AddPillButton/AddPillButton';
 import PillboxButton from '../PillboxButton/PillboxButton';
 
-export default class WelcomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = null;
-  }
+const WelcomePage = ({ addPillClick, viewPillboxClick }) => (
+  <div>
+    <h1>welcome to pillbox</h1>
+    <AddPillButton addPillClick={addPillClick} />
+    <PillboxButton viewPillboxClick={viewPillboxClick} />
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <h1>welcome to pillbox</h1>
-        <AddPillButton addPillClick={this.props.addPillClick} />
-        <PillboxButton viewPillboxClick={this.props.viewPillboxClick} />
-      </div>
-    );
-  }
-}
+WelcomePage.defaultProps = {
+  addPillClick: 'Cannot get add pills',
+  viewPillboxClick: 'Cannot view pillbox',
+};
+
+WelcomePage.propTypes = {
+  addPillClick: PropTypes.func,
+  viewPillboxClick: PropTypes.func,
+};
+
+export default WelcomePage;
