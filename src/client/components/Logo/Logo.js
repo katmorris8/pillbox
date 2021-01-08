4a0ef5fbecd30f7e914e8b2da5ba003350b4fc0a
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-07 21:06:54 -0500

    convert to arrow function

--- src/client/components/Logo/Logo.js
+++ src/client/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
 
-export function Logo() {
-  return <h1 className="logo">Pillbox</h1>;
-}
+const Logo = () => <h1 className="logo">Pillbox</h1>;
+
+export default Logo;
