d6aa7b86706908cbb6a8c66587deb3526290c1f3
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-11 19:58:14 -0500

    format doc

--- src/server/controllers/prescriptions.js
+++ src/server/controllers/prescriptions.js
@@ -2,7 +2,7 @@ const { Prescription } = require('../models/prescription');
 
 const prescriptionsController = {
   index: async () => {
-    const prescription = await Prescription.findAll({ attributes: ['id', 'name'] })
+    const prescription = await Prescription.findAll({ attributes: ['id', 'name'] });
     return prescription;
   },
   create: params => Prescription.create(params.prescription)
