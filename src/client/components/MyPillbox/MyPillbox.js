a72865b4dd98fc331e072b0513d47db3d5040db8
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-11 19:39:50 -0500

    add state in pillbox. update the pillbox state with the pills props. render the pills state

--- src/client/components/MyPillbox/MyPillbox.js
+++ src/client/components/MyPillbox/MyPillbox.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const MyPillbox = ({ pills }) => (
-  <div>
-    {pills.map((pill, i) => (
-      <p key={i}>{pill.name}</p>
-    ))}
-  </div>
-);
+const MyPillbox = ({ pills }) => {
+  const [pillboxPills, setPillboxPills] = useState(pills);
+
+  useEffect(() => {
+    setPillboxPills(pills);
+  }, [pillboxPills]);
+
+  return (
+    <div>
+      {pillboxPills.map((pill, i) => (
+        <p key={i}>{pill}</p>
+      ))}
+    </div>
+  );
+};
 
 MyPillbox.defaultProps = {
   pills: [],
