9f2c6e10662d54c1abe8825c71f3f235ced35c2f
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-12 21:07:04 -0500

    remove state out of my pillbox

--- src/client/components/MyPillbox/MyPillbox.js
+++ src/client/components/MyPillbox/MyPillbox.js
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const MyPillbox = ({ pills }) => {
-  const [pillboxPills, setPillboxPills] = useState(pills);
-
-  useEffect(() => {
-    setPillboxPills(pills);
-  }, [pillboxPills]);
-
-  return (
-    <div>
-      {pillboxPills.map((pill, i) => (
-        <p key={i}>{pill}</p>
-      ))}
-    </div>
-  );
-};
+const MyPillbox = ({ pills }) => (
+  <div>
+    {pills.map((pill, i) => (
+      <p key={i}>{pill}</p>
+    ))}
+  </div>
+);
 
 MyPillbox.defaultProps = {
   pills: [],
