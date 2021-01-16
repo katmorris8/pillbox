47f4ab3570fd12199a14ed9b0060b40a55a27104
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-16 12:17:41 -0500

    convert to functional component with its own state. update the change and submit functions to update the new state.

--- src/client/components/PillSearch/PillSearch.js
+++ src/client/components/PillSearch/PillSearch.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
-import request from '../../helpers/request';
+import React, { useState } from 'react';
+// import request from '../../helpers/request';
 
-export default class PillSearch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.initialState();
-  }
+const PillSearch = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = this.initialState();
+  // }
 
-  initialState = () => {
-    return {
-      name: ''
-    };
-  }
+  // initialState = () => {
+  //   return {
+  //     name: ''
+  //   };
+  // }
 
-  handleSubmit = async (e) => {
+  // handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const response = await request.createPill({ name: this.state.name });
+  //   if (response.status === 200) {
+  //     this.setState(
+  //       this.initialState()
+  //     );
+  //   }
+  // }
+
+  const [state, setState] = useState('');
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setState(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const response = await request.createPill({ name: this.state.name });
-    if (response.status === 200) {
-      this.setState(
-        this.initialState()
-      );
-    }
-  }
+    console.log('submitted', state);
+  };
 
-  pillName = (e) => {
-    this.setState({
-      name: e.target.value
-    });
-  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Pill name..."
+          onChange={handleChange}
+          value={state}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          &gt;
+        </button>
+      </form>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Pill name..."
-            onChange={this.pillName}
-            value={this.state.name}
-          />
-          <button type="submit" onClick={this.handleSubmit}>></button>
-        </form>
-      </div>
-    );
-  }
-}
+export default PillSearch;
