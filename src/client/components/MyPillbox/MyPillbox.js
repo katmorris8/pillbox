008d3f5538857301f43d307fabc4134810b1ed73
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-24 20:06:58 -0500

    add state to mypillbox

--- src/client/components/MyPillbox/MyPillbox.js
+++ src/client/components/MyPillbox/MyPillbox.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const MyPillbox = ({ pills }) => (
-  <div>
-    {pills.map(pill => (
-      <div key={pill.id} className="pill">
-        <button type="button">x</button>
-        <p>{pill.name}</p>
-      </div>
-    ))}
-  </div>
-);
+const MyPillbox = ({ pills }) => {
+  const [state, setState] = useState('');
+
+  return (
+    <div>
+      {pills.map(pill => (
+        <div key={pill.id} className="pill">
+          <button type="button">x</button>
+          <p>{pill.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 MyPillbox.defaultProps = {
   pills: [],
